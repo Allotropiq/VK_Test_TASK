010e64f861bea035429bfa67031af63a60336f26
Allotropiq <allotropiq@gmail.com>
2025-11-28 22:27:41 +0300

    1monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 CONFFILE="./source/configuration.txt"
-SITEURL=$(grep "Адрес сайта=" $CONFFILE | awk -F'=' '{print $2}' | tr -d ' \t\r\n')
+SITEURL=$(grep "Адрес сайта проверяемого сайта=" $CONFFILE | awk -F'=' '{print $2}' | tr -d ' \t\r\n')
 INT=$(grep "Интервал запуска в секундах=" $CONFFILE | awk -F'=' '{print $2}' | tr -d ' \t\r\n')
 errorFlag=0
 
@@ -8,22 +8,23 @@ errorFlag=0
 if [[ ! "$INT" =~ ^[0-9]+$ ]]; then
     echo "Новое значение не применено, будет использовано изначальное. $(date '+%Y-%m-%d %H:%M:%S')" | tee -a monitoring.log;
     errorFlag=1;
-elif $1 -eq $int; then 
+elif [ $1 -eq $INT ]; then 
     INT=$1
 else
-    exec checkscript.sh
+   nohup exec ./checkscript.sh &
 fi
 
-if ! docker ps | grep mini_server; then 
-    docker compose up -d;
+if ! docker ps | grep -q -e mini_server ; then
+    pushd ./source/ &&  docker compose up -d && popd;
     echo "Запуск контейнера с приложением $(date '+%Y-%m-%d %H:%M:%S')" | tee -a monitoring.log;
 else 
-    if curl -sI "$SITEURL" | grep -q -e "HTTP/.* 2.." -e "HTTP/.* 3.."; then 
-        echo "Сайт доступен! $(date '+%Y-%m-%d %H:%M:%S')"
-    elif curl -sI "$SITEURL" | grep -q -e "HTTP/.* 4.."; then
+    if curl -sI "$SITEURL" | grep -q -e "HTTP/.* 2.." -e "HTTP/.* 3.." ; then 
+        echo "Сайт доступен! $(date '+%Y-%m-%d %H:%M:%S')" | tee -a monitoring.log;
+    elif curl -sI "$SITEURL" | grep -q -e "HTTP/.* 4.." ; then
         echo "Неполадки на стороне клиента. $(date '+%Y-%m-%d %H:%M:%S')" | tee -a monitoring.log;
     else 
         echo "Неполадки на стороне сервера. Попытка перезагрузки. $(date '+%Y-%m-%d %H:%M:%S')" | tee -a monitoring.log;
-        docker compose restart
+        pushd ./source/ &&  docker compose restart && popd;
+        sleep 60
     fi
-fi
+fi 
