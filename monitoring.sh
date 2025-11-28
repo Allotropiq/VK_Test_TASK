2718c37fa95dcace940a13fd88b7ff683bde0f83
Allotropiq <allotropiq@gmail.com>
2025-11-28 21:27:25 +0300

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -16,14 +16,14 @@ fi
 
 if ! docker ps | grep mini_server; then 
     docker compose up -d;
-    echo "Запуск контейнера с приложением $(date '+%Y-%m-%d %H:%M:%S')"
+    echo "Запуск контейнера с приложением $(date '+%Y-%m-%d %H:%M:%S')" | tee -a ../monitoring.log;
 else 
     if curl -sI "$SITEURL" | grep -q -e "HTTP/.* 2.." -e "HTTP/.* 3.."; then 
         echo "Сайт доступен! $(date '+%Y-%m-%d %H:%M:%S')"
     elif curl -sI "$SITEURL" | grep -q -e "HTTP/.* 4.."; then
-        echo "Неполадки на стороне клиента. $(date '+%Y-%m-%d %H:%M:%S')" 
+        echo "Неполадки на стороне клиента. $(date '+%Y-%m-%d %H:%M:%S')" | tee -a ../monitoring.log;
     else 
-        echo "Неполадки на стороне сервера. Попытка перезагрузки. $(date '+%Y-%m-%d %H:%M:%S')" 
+        echo "Неполадки на стороне сервера. Попытка перезагрузки. $(date '+%Y-%m-%d %H:%M:%S')" | tee -a ../monitoring.log;
         docker compose restart
     fi
 fi
